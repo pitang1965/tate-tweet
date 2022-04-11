66ce9fbeedf637fec53181b4117dc58e65d368dd
Shoji Makino <pitang1965@gmail.com>
2022-04-11 13:56:47 +0900

    Introduce Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,79 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { createStyles, Button, Stack, Textarea, Title } from '@mantine/core';
+import { useClipboard, useDebouncedValue } from '@mantine/hooks';
+
+const useStyles = createStyles((/* theme, _params, getRef */) => ({
+  textarea: {
+    width: '90%',
+  },
+}));
+
+const reverceString = (text: string) => text.split('').reverse().join('');
+
+const halfToFullWidthCharacters = (text: string) =>
+  text.replace(/[!-~]/g, (s) => String.fromCharCode(s.charCodeAt(0) + 0xfee0));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { classes } = useStyles();
+
+  const clipboard = useClipboard({ timeout: 500 });
+  const handleCopy = () => {
+    clipboard.copy(debounced);
+  };
+
+  const [tweet, setTweet] = useState('');
+  const [debounced] = useDebouncedValue(
+    halfToFullWidthCharacters(reverceString(tweet)),
+    200
+  );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <Title order={1}>
+          たてツイート
+        </Title>
+        <p>ツイートを縦書きに変換します。</p>
+        <Textarea
+          placeholder='文章を入力してください。'
+          label='ツイートを入力'
+          radius='xs'
+          size='lg'
+          required
+          value={tweet}
+          onChange={(event) => setTweet(event.currentTarget.value)}
+          className={classes.textarea}
+        />
+        <Textarea
+          placeholder='縦書きで表示されます'
+          label='縦書きツイート'
+          radius='xs'
+          size='lg'
+          disabled
+          value={debounced}
+          className={classes.textarea}
+        />
+        <Button
+          variant='gradient'
+          gradient={{ from: 'indigo', to: 'cyan' }}
+          onClick={handleCopy}
+        >
+          Copy
+        </Button>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://over40web.club'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
+            Powerd by Over 40 Web Club
           </a>
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MantineProvider } from '@mantine/core';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={{
+        colorScheme: 'dark',
+        fontFamily:
+          '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+      }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
