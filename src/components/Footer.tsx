26df5ffb17a4101fd566bbfd810461b642c6b8cc
Shoji Makino <pitang1965@gmail.com>
2022-04-13 21:11:24 +0900

    Fix folder name (Components and components)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Anchor, Footer as MantineFooter } from '@mantine/core';
+const Footer = () => {
+  return (
+    <MantineFooter height={60} p='md'>
+    <Anchor href='https://over40web.club' target='_blank' rel='noopener noreferrer'>
+      Powerd by Over 40 Web Club
+    </Anchor>
+  </MantineFooter>
+  )
+}
+
+export default Footer
