6e2e0b341cba079cc2af0f611c8f79310d204253
Shoji Makino <pitang1965@gmail.com>
2022-04-14 20:14:43 +0900

    Fix: Abnormal termination when entering an emoji

--- src/lib/convTweet.ts
+++ src/lib/convTweet.ts
@@ -1,40 +1,57 @@
+import GraphemeSplitter from 'grapheme-splitter';
+
+const splitter = new GraphemeSplitter();
+
+// 文字が改行文字かどうか
+const isNewLineCharacter = (aChar: string) =>
+  aChar[0] === '\r' || aChar[0] === '\n';
+
+// 文字が半角かどうか
+const isHalfWidthChar = (aChar: string) => aChar[0] >= '!' && aChar[0] <= '~';
+
 export const reverceString = (text: string) =>
   text.split('').reverse().join('');
 
 const halfToFullWidthCharacters = (text: string) =>
   text.replace(/[!-~]/g, (s) => String.fromCharCode(s.charCodeAt(0) + 0xfee0));
 
-export const getCharLength = (str: string) => {
-  str = str === null ? '' : str;
-  return str
-    .split('')
-    .reduce(
-      (previous, current) =>
-        encodeURI(current).length > 1 && encodeURI(current) !== '%0A'
-          ? previous + 1
-          : previous + 0.5,
-      0
-    );
+// 文字数カウントのルール
+// 改行と半角は0.5、全角と絵文字は1
+export const getCharLength = (str: string): number => {
+  if (str === null) return 0;
+  const split: string[] = splitter.splitGraphemes(str);
+  const charCount = split.reduce(
+    (prev, current) =>
+      isNewLineCharacter(current) || isHalfWidthChar(current)
+        ? prev + 0.5
+        : prev + 1.0,
+    0
+  );
+  return charCount;
 };
 
-export const conv2TateTweet = (str: string, wideLineSpacing:'none'|'half'|'full' = 'full') => {
+export const conv2TateTweet = (
+  str: string,
+  wideLineSpacing: 'none' | 'half' | 'full' = 'full'
+) => {
   let stringArray: string[][] = [];
 
   // 文字列を全角に変換した上で、各行、各文字を二次元配列に格納する
   let existString = false;
-  halfToFullWidthCharacters(str)
-    .split('')
-    .map((value) => {
-      if (!existString) {
-        stringArray.push(new Array());
-        existString = true;
-      }
-      if (isNewLineCharacter(value)) {
-        existString = false;
-      } else {
-        stringArray[stringArray.length - 1].push(value);
-      }
-    });
+  const fullWidthCharacters = halfToFullWidthCharacters(str);
+  const splittedCharacters: string[] =
+    splitter.splitGraphemes(fullWidthCharacters);
+  splittedCharacters.map((value) => {
+    if (!existString) {
+      stringArray.push(new Array());
+      existString = true;
+    }
+    if (isNewLineCharacter(value)) {
+      existString = false;
+    } else {
+      stringArray[stringArray.length - 1].push(value);
+    }
+  });
 
   // 最大配列サイズを取得
   const colSize = stringArray.reduce(
@@ -57,20 +74,18 @@ export const conv2TateTweet = (str: string, wideLineSpacing:'none'|'half'|'full'
   for (let col = 0; col < colSize; col++) {
     for (let row = spaceAddedStringArray.length - 1; row >= 0; row--) {
       finalString += spaceAddedStringArray[row][col];
-      if (wideLineSpacing === 'half') {
-        finalString += ' ';
-      } else if (wideLineSpacing === 'full') {
-        finalString += '　';
+      if (row !== 0) {
+        if (wideLineSpacing === 'half') {
+          finalString += ' ';
+        } else if (wideLineSpacing === 'full') {
+          finalString += '　';
+        }
       }
     }
-    finalString += '\n';
+    if (col < colSize - 1) {
+      finalString += '\n';
+    }
   }
 
   return finalString;
 };
-
-// 文字が改行文字かどうか調べる
-function isNewLineCharacter(aChar: string) {
-  const firstChar = aChar[0];
-  return firstChar === '\r' || firstChar === '\n';
-}
