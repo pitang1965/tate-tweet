10768d5e92e10dd2146372be6f078284c9f6c613
Shoji Makino <pitang1965@gmail.com>
2022-04-23 21:18:45 +0900

    Add icons to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,51 @@
 import type { FC } from 'react';
-import { Anchor, Navbar as MantineNavbar } from '@mantine/core';
+import { createStyles, Anchor, Navbar as MantineNavbar } from '@mantine/core';
 import { Link } from 'react-router-dom';
+import { BrandTwitter, InfoCircle } from 'tabler-icons-react';
+
+const useStyles = createStyles((theme /*, _params, getRef */) => ({
+  anchor: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '0.5rem',
+    textDecoration: 'none',
+    '&:hover': {
+      color: theme.fn.darken('#00acee', 0.7),
+      textDecoration: 'none',
+    },
+  },
+}));
 
 type NavbarProps = {
   opened: boolean;
 };
 
-const Navbar: FC<NavbarProps> = ({ opened }) => (
-  <>
-    <MantineNavbar
-      p='md'
-      hiddenBreakpoint='sm'
-      hidden={!opened}
-      width={{ sm: 200, lg: 300 }}
-    >
-      <MantineNavbar.Section mt='xs'>
-        <Anchor component={Link} to='/'>
-          Tweet
-        </Anchor>
-      </MantineNavbar.Section>
-      <MantineNavbar.Section mt='xs'>
-        <Anchor component={Link} to='about'>
-          About
-        </Anchor>
-      </MantineNavbar.Section>
-    </MantineNavbar>
-  </>
-);
+const Navbar: FC<NavbarProps> = ({ opened }) => {
+  const { classes } = useStyles();
+
+  return (
+    <>
+      <MantineNavbar
+        p='md'
+        hiddenBreakpoint='sm'
+        hidden={!opened}
+        width={{ sm: 200, lg: 300 }}
+      >
+        <MantineNavbar.Section mt='xs'>
+          <Anchor component={Link} to='/' className={classes.anchor}>
+            <BrandTwitter />
+            <div>Tweet</div>
+          </Anchor>
+        </MantineNavbar.Section>
+        <MantineNavbar.Section mt='xs'>
+          <Anchor component={Link} to='about' className={classes.anchor}>
+            <InfoCircle />
+            <div>About</div>
+          </Anchor>
+        </MantineNavbar.Section>
+      </MantineNavbar>
+    </>
+  );
+};
 
 export default Navbar;
