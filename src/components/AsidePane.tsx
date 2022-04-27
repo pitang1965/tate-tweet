4b469ccc0725aba9afdd4b8db4aa6e4596e42b6e
Shoji Makino <pitang1965@gmail.com>
2022-04-27 21:46:06 +0900

    Chage book ads

--- src/components/AsidePane.tsx
+++ src/components/AsidePane.tsx
@@ -9,7 +9,17 @@ const AsidePane = () => {
       <Text size='md'>とりあえず何か追加してみました。</Text>
       <BookAd
         code={
-          '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pitang1965-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=499085652X&linkId=7071bd9ad0005dee8a2b112b0970b0dd"></iframe>'
+          '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pitang1965-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=428420470X&linkId=8e7ec18c9069c4089118a78bcae653bc"></iframe>'
+        }
+      />
+      <BookAd
+        code={
+          '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pitang1965-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4898318371&linkId=94eab0cec23dc720ea92b619da171b36"></iframe>'
+        }
+      />
+      <BookAd
+        code={
+          '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pitang1965-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4828423710&linkId=2434b093123c773206673c4be8a77256"></iframe>'
         }
       />
     </>
