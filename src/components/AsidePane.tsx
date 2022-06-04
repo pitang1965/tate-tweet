550bd421e57f57ff16e764ff2b48fda87b6a72f3
Shoji Makino <pitang1965@gmail.com>
2022-06-04 17:11:40 +0900

    Add Container to AsidePane

--- src/components/AsidePane.tsx
+++ src/components/AsidePane.tsx
@@ -1,12 +1,18 @@
-import { Text } from '@mantine/core';
+import { Container, Text } from '@mantine/core';
 import { Ad } from 'tabler-icons-react';
 import BookAd from './BookAd';
 
 const AsidePane = () => {
   return (
-    <>
+    <Container>
       <Ad size={40} strokeWidth={1} color={'#1C92E2'} />
       <Text size='md'>とりあえず何か追加してみました。</Text>
+      <BookAd
+        code={
+          '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pitang1965-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B09YQ6S2K9&linkId=153ea12824af1caa002e26bd013843b7"></iframe>'
+        }
+      />
+
       <BookAd
         code={
           '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pitang1965-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4198654530&linkId=2c69f7ba78c95fcfd2f39b3615c8f18a"></iframe>'
@@ -22,7 +28,7 @@ const AsidePane = () => {
           '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pitang1965-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4828423710&linkId=2434b093123c773206673c4be8a77256"></iframe>'
         }
       />
-    </>
+    </Container>
   );
 };
 
