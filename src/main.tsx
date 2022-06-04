c3489eee8c6677fc5461104c54ebdf9895d85b60
Shoji Makino <pitang1965@gmail.com>
2022-06-04 19:22:38 +0900

    Update to Client Rendering API in React 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { MantineProvider, Global } from '@mantine/core';
 import App from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
     <MantineProvider
       theme={{
@@ -21,6 +23,5 @@ ReactDOM.render(
       />
       <App />
     </MantineProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
